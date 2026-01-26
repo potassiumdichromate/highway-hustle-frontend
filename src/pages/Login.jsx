7006d0c2fb0dbaf5873b66d677961b43ddaaac07
BigClitPhobia <sidhanth6am@gmail.com>
2026-01-26 10:12:00 +0700

    Final

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -117,7 +117,7 @@ export default function Login() {
             {/* Kult Games Logo */}
             <div className="kult-logo-container">
               <img 
-                src={KultLogo} 
+                src={OGPurpleLogo} 
                 alt="Kult Games" 
                 className="kult-logo"
               />
